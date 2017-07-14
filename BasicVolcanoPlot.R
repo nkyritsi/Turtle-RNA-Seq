d0d76a100a25486ab3690179607d2ffb17cedaef
Nia Kyritsis <nkyritsi@fandm.edu>
2017-07-14 10:24:37 -0400

    Update BasicVolcanoPlot.R

--- BasicVolcanoPlot.R
+++ BasicVolcanoPlot.R
@@ -7,7 +7,7 @@ gonadDF <- read.delim(file = inputFile, sep = ",")
 ###PostFC and FDR are column labels
 with(gonadDF, plot(log2(PostFC), -log10(FDR), pch=20, main="Volcano plot", xlim = c(-2.5, 2)))
 
-##Add colored points; red if p<0.05, orange if log2FC>1.5, green if both
+##Add colored points; red if p<0.05, orange if log2FC>0.5849625 (FC > 1.5), green if both
 with(subset(gonadDF, FDR<.05), points(log2(PostFC), -log10(FDR), pch=20, col="red"))
-with(subset(gonadDF, abs(log2(PostFC))>1), points(log2(PostFC), -log10(FDR), pch=20, col="orange"))
-with(subset(gonadDF, FDR<0.05 & abs(log2(PostFC))>1), points(log2(PostFC), -log10(FDR), pch=20, col="green"))
+with(subset(gonadDF, abs(log2(PostFC))>0.5849625), points(log2(PostFC), -log10(FDR), pch=20, col="orange"))
+with(subset(gonadDF, FDR<0.05 & abs(log2(PostFC))>0.5849625), points(log2(PostFC), -log10(FDR), pch=20, col="green"))
